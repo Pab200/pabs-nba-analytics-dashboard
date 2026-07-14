ce33c5f76adc64200489d4f1d7f3009ef218c0ca
Pablo Miller Niharra <pmiller@virtualdesk.es>
2026-07-14 12:52:38 +0200

    added a sql database and another analysis notebook that utalizes SQL.

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 Super Duper cool NBA Analytics Dashboard -
 
 A personal project analyzing NBA data using Python, Pandas, SQL, and interactive visualizations for learning
+
+Where are we at:
+- We have created our first Notebooks for getting the given Nba data of which we have
+    - players.csv: a very vague list of all players in the history of the NBA
+    - rookies.csv: a list of the 2025 NBA Draft Class among other details
+    - season_stats_2025-26.csv: this is an extensive table of statistics of all players in the 2025-26 season
+    - teams.csv: another vague list of NBA teams
+- We have created a data base using Python, and will use SQL to further do edits and other analysis with the data we have gotten
+- Created another notebook to do analysis using SQL as well as a test of MadLibs (whether we will use more of MadLibs in the future or not we will find out)
---
+++ src/create_database.py
@@ -0,0 +1,39 @@
+# 1. Load CSVs
+import pandas as pd
+import sqlite3
+import os
+
+DATA_DIR = "data/raw"
+
+players = pd.read_csv(f"{DATA_DIR}/players.csv")
+teams = pd.read_csv(f"{DATA_DIR}/teams.csv")
+rookies = pd.read_csv(f"{DATA_DIR}/rookies.csv")
+season_stats = pd.read_csv(f"{DATA_DIR}/season_stats_2025-26.csv")
+
+print("Players: ", players.shape)
+print("Teams: ", teams.shape)
+print("Rookies: ", rookies.shape)
+print("Season Stats: ", season_stats.shape)
+
+# 2. Connect to SQLite (create nba.db if missing)
+DB_PATH = "nba.db"
+
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+
+# 3. Create tables
+# 4. Insert DataFrames into tables
+players.to_sql("players", conn, if_exists="replace", index=False)
+teams.to_sql("teams", conn, if_exists="replace", index=False)
+rookies.to_sql("rookies", conn, if_exists="replace", index=False)
+season_stats.to_sql("season_stats", conn, if_exists="replace", index=False)
+
+# 5. Print verification
+print("Database created successfully!")
+print("Players table:", len(players))
+print("Teams table:", len(teams))
+print("Rookies table:", len(rookies))
+print("Season Stats table:", len(season_stats))
+
+# 6. Close connection
+conn.close()
